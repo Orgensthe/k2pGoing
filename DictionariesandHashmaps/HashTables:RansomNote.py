6d5d862637f57b456bc29fb4a7535b775ffbb2a9
jeff <w4102945@gmail.com>
2020-08-04 10:00:46 +0900

    Jeff : Complete Hash Tables: Ransom Note

--- DictionariesandHashmaps/HashTables:RansomNote.py
+++ DictionariesandHashmaps/HashTables:RansomNote.py
@@ -1,1 +1,25 @@
 def checkMagazine(magazine, note):
+
+
+
+    dic = dict()
+    for word in magazine:
+        if word in dic:
+            dic[word] +=1
+        else:
+            dic[word] = 1
+    try:
+        for word in note:
+            dic[word]-=1
+            if dic[word] < 0 :
+                print("No")
+                return
+        print("Yes")
+    except KeyError:
+        print("No")
+
+
+
+
+
+checkMagazine(["two","times","three","is","not","four"],["two","times","two","is","four"])
