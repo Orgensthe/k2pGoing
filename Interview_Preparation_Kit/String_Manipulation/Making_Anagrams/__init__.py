97983e331cf1d8996e357d2ca8af72c09b2b9ed1
shlee <ac2dia>
2020-08-05 22:43:26 +0900

    Strings: Making Anagrams

--- Interview_Preparation_Kit/String_Manipulation/Making_Anagrams/__init__.py
+++ Interview_Preparation_Kit/String_Manipulation/Making_Anagrams/__init__.py
@@ -8,7 +8,15 @@ import sys
 
 # Complete the makeAnagram function below.
 def makeAnagram(a, b):
-    pass
+    alphabet_list = [0 for _ in range(26)]
+    for alpha in a:
+        alphabet_list[ord(alpha)-97] += 1
+    for alpha in b:
+        alphabet_list[ord(alpha)-97] -= 1
 
+    anagrams = 0
+    for alpha in alphabet_list:
+        anagrams += abs(alpha)
+    print(anagrams)
 
 makeAnagram("cde", "abc")
