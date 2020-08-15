0db9d9235be37d3aaf4dacbbab0c6167883009d0
박진우 <jellywing@innogrid.com>
2020-08-15 11:32:35 +0900

    Hash Table: Ice Cream Parlor

---
+++ Hash Tables: Ice Cream Parlor/src/Solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Solution {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+        Map<Integer, Integer> costMap = new HashMap<>();
+
+        for (int i = 0; i < cost.length; i++) {
+            if (costMap.containsKey(money - cost[i])) {
+                System.out.println((costMap.get(money - cost[i]) + 1) + " " + (i + 1));
+                break;
+            } else
+                costMap.put(cost[i], i);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
