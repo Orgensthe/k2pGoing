dffa4107c32860d05664adb6221032fc9b9e6bd6
JEONGYUNHA <jhy3990585@naver.com>
2020-09-22 23:15:40 +0900

    GreedyFlorist

---
+++ GreedyFlorist/src/Solution.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the getMinimumCost function below.
+    static int getMinimumCost(int k, int[] c) {
+
+        int price = 0;
+
+        Arrays.sort(c);
+
+        for(int i=0;i<c.length;i++){
+            int num = i/k;
+            price += (num+1)*c[c.length-(i+1)];
+        }
+
+        return price;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]); // 꽃 개수
+
+        int k = Integer.parseInt(nk[1]); // 친구 수
+
+        int[] c = new int[n]; // 각 꽃의 가격
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int minimumCost = getMinimumCost(k, c);
+
+        bufferedWriter.write(String.valueOf(minimumCost));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
